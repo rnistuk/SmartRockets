fd564c84be20836ffb0f2636f6100f7e408ca481
Rich Nistuk <rnistuk@gmail.com>
2017-07-23 12:33:04 -0700

    Added basic draw call timing functionality.

--- source/sketch.js
+++ source/sketch.js
@@ -1,9 +1,14 @@
 var population;
 var lifespan = 400;
 var lifeP;
+var drawTimeP;
 var count = 0;
+var populationCount = 0;
 var target;
 var maxForce = 0.2;
+
+const drawStats = new Statistics();
+const popStats = new Statistics(); 
 
 var rx = 100;
 var ry = 150;
@@ -15,15 +20,21 @@ function setup() {
     population = new Population();
     lifeP = createP();
     target = createVector( width/2, 50);
+    drawTimeP = createP();
 }
 
 function draw() {
+    const startTime = performance.now();
+    
     background(0);
     population.run();
-    lifeP.html(count);
-    
     count++;
+    
+    lifeP.html(`Population: ${populationCount}   life count:${count}`);
+    
+    
     if (count === lifespan) {
+        populationCount++;
         population.evaulate();
         population.selection();
         count = 0;
@@ -31,8 +42,41 @@ function draw() {
     
     fill(255);
     rect(100,150, 200,10);
-    
     ellipse(target.x, target.y,16,16);
+    
+    
+    populationCount > 3 ? updatePerformanceStats(startTime) : drawTimeP.html("Ignoring stats for first 3 populations.");
+    if(populationCount>3 && (populationCount % 10 === 0) && (count===0)) {
+        const instantPerfStatP = createP();
+        instantPerfStatP.html(reportDrawStats(drawStats));
+        
+    }
+    
+    function updatePerformanceStats(startTime) {
+        drawStats.update((performance.now()) - startTime);
+        drawTimeP.html(reportDrawStats(drawStats));
+    }
+    
+    function reportDrawStats(drawStats) {
+        return `Average draw time at population: ${populationCount} --  ${drawStats.ave().toFixed(2)}ms min:${drawStats.min.toFixed(2)}ms max:${drawStats.max.toFixed(2)}ms`;
+        
+    }
+    
+}
+
+function Statistics() {
+    this.count = 0;
+    this.total = 0.0;
+    this.min = 100000.0;
+    this.max = 0.0;
+    
+    this.update = (v) => {
+        this.total += v;
+        this.count++;
+        this.min = v < this.min ? v : this.min;
+        this.max = v > this.max ? v : this.max;
+    }
+    this.ave = () => (this.total/this.count);
 }
 
 function Population() {
@@ -115,7 +159,7 @@ function DNA(genes) {
     
     this.mutation = function() {
         for (var i=0; i< this.genes.length; ++i) {
-            if(random(1) < 0.1) {
+            if(random(1) < 0.01) {
                 this.genes[i] =p5.Vector.random2D();
                 this.genes[i].setMag(maxForce);
             }
